2e696ad4688ca360632d66c479276418573ff6b0
Kenichi Kamiya <kachick1@gmail.com>
2023-07-16 03:20:09 +0900

    Add failing tests first

---
+++ testdata/candidates/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/e57b65abbbf7a2d5786acc86fdf56cde060ed026";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              go_1_20
+              go-task
+              goreleaser
+            ];
+          };
+      });
+}
---
+++ testdata/classic/default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e57b65abbbf7a2d5786acc86fdf56cde060ed026.tar.gz") { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.go_1_20
+    pkgs.go-task
+    pkgs.goreleaser
+  ];
+}
